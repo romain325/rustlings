063cbfedd55348e61db403cb277a3a476da16bf3
Romain OLIVIER <rolivier@cirilgroup.com>
2022-01-19 08:31:46 +0100

    cookie

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -33,10 +33,18 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let arr : Vec<&str> = s.split(",").collect();
+        if arr.len() != 2 || arr[0].is_empty() || arr[1].is_empty() {
+            Person::default()
+        } else {
+            let age = match arr[1].parse::<usize>() {
+                Ok(age) => return Person { name: arr[0].to_string(), age: age },
+                Err(err) => return Person::default(),
+            };
+        }
+
     }
 }
 
