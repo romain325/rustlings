0446b0936bb26cadd2c32b23f439d31628d64922
Romain OLIVIER <rolivier@cirilgroup.com>
2022-01-20 08:19:42 +0100

    finished

--- exercises/advanced_errors/advanced_errs2.rs
+++ exercises/advanced_errors/advanced_errs2.rs
@@ -16,8 +16,6 @@
 // 4. Complete the partial implementation of `Display` for
 //    `ParseClimateError`.
 
-// I AM NOT DONE
-
 use std::error::Error;
 use std::fmt::{self, Display, Formatter};
 use std::num::{ParseFloatError, ParseIntError};
@@ -46,24 +44,29 @@ impl From<ParseIntError> for ParseClimateError {
 // `ParseFloatError` values.
 impl From<ParseFloatError> for ParseClimateError {
     fn from(e: ParseFloatError) -> Self {
-        // TODO: Complete this function
+        // Complete this function
+        Self::ParseFloat(e)
     }
 }
 
-// TODO: Implement a missing trait so that `main()` below will compile. It
+// Implement a missing trait so that `main()` below will compile. It
 // is not necessary to implement any methods inside the missing trait.
+impl Error for ParseClimateError {}
 
 // The `Display` trait allows for other code to obtain the error formatted
 // as a user-visible string.
 impl Display for ParseClimateError {
-    // TODO: Complete this function so that it produces the correct strings
+    // Complete this function so that it produces the correct strings
     // for each error variant.
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        // Imports the variants to make the following code more compact.
         use ParseClimateError::*;
         match self {
             NoCity => write!(f, "no city name"),
+            Empty => write!(f, "empty input"),
+            BadLen => write!(f, "incorrect number of fields"),
+            ParseInt(e) => write!(f, "error parsing year: invalid digit found in string"),
             ParseFloat(e) => write!(f, "error parsing temperature: {}", e),
+            _ => write!(f, "unhandled error!"),
         }
     }
 }
@@ -85,9 +88,12 @@ struct Climate {
 // 6. Return an `Ok` value containing the completed `Climate` value.
 impl FromStr for Climate {
     type Err = ParseClimateError;
-    // TODO: Complete this function by making it handle the missing error
+    // Complete this function by making it handle the missing error
     // cases.
     fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.len() == 0 {
+            return Err(Self::Err::Empty);
+        }
         let v: Vec<_> = s.split(',').collect();
         let (city, year, temp) = match &v[..] {
             [city, year, temp] => (city.to_string(), year, temp),
@@ -95,7 +101,11 @@ impl FromStr for Climate {
         };
         let year: u32 = year.parse()?;
         let temp: f32 = temp.parse()?;
-        Ok(Climate { city, year, temp })
+        if city.len() == 0 {
+            Err(Self::Err::NoCity)
+        } else {
+            Ok(Climate { city, year, temp })
+        }
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -26,8 +26,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -41,6 +39,24 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(ParsePersonError::Empty);
+        }
+
+        let arr : Vec<&str> = s.split(",").collect();
+        if arr.len() != 2 {
+            return Err(ParsePersonError::BadLen);
+        }
+
+        if arr[0].is_empty() {
+            return Err(ParsePersonError::NoName);
+        }
+
+        let age = match arr[1].parse::<usize>() {
+            Ok(age) => return Ok(Person { name: arr[0].to_string(), age: age }),
+            Err(err) => return Err(ParsePersonError::ParseInt(err)),
+        };
+
     }
 }
 
